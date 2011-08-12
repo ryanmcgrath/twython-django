bcfb58db8f3d138013773cf00466f5d7fc85af99
ArvinJA <arvin@arvtard.com>
2011-08-13 02:18:41 +0300

    Added quotes so the functions wouldn't be called where they hadn't yet been defined.

--- twython_django_oauth/urls.py
+++ twython_django_oauth/urls.py
@@ -4,16 +4,16 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('your_app.twython_django_oauth.views',
 	
 	# First leg of the authentication journey...
-	(r'^login/?$', begin_auth),
+	(r'^login/?$', "begin_auth"),
 
 	# Logout, if need be
-	(r'^/logout?$', logout), # Calling logout and what not
+	(r'^/logout?$', "logout"), # Calling logout and what not
 	
 	# This is where they're redirected to after authorizing - we'll
 	# further (silently) redirect them again here after storing tokens and such.
-	(r'^thanks/?$', thanks),
+	(r'^thanks/?$', "thanks"),
 
 	# An example view using a Twython method with proper OAuth credentials. Clone
 	# this view and url definition to get the rest of your desired pages/functionality.
-	(r'^user_timeline/?$', user_timeline),
+	(r'^user_timeline/?$', "user_timeline"),
 )
