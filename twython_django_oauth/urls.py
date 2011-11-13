50ac5263b7c6df3af55fe8ea9a631c4d8404c2f0
Andrew Cassidy <andee@bytz.co.uk>
2011-11-13 14:48:46 +0000

    added url names for easier external lookup

--- twython_django_oauth/urls.py
+++ twython_django_oauth/urls.py
@@ -4,16 +4,16 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('twython_django_oauth.views',
 	
 	# First leg of the authentication journey...
-	(r'^login/?$', "begin_auth"),
+	(r'^login/?$', "begin_auth", name="twitter_login"),
 
 	# Logout, if need be
-	(r'^logout/?$', "logout"), # Calling logout and what not
+	(r'^logout/?$', "logout", name="twitter_logout"), # Calling logout and what not
 	
 	# This is where they're redirected to after authorizing - we'll
 	# further (silently) redirect them again here after storing tokens and such.
-	(r'^thanks/?$', "thanks"),
+	(r'^thanks/?$', "thanks", name="twitter_callback"),
 
 	# An example view using a Twython method with proper OAuth credentials. Clone
 	# this view and url definition to get the rest of your desired pages/functionality.
-	(r'^user_timeline/?$', "user_timeline"),
+	(r'^user_timeline/?$', "user_timeline", name="twitter_timeline"),
 )
