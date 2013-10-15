6cc80cc4c6ddce4ddc52b99041ef8bc5ccb92768
Mike Helmick <mikeh@ydekproductions.com>
2013-10-15 11:05:16 -0400

    Fix setup.py and MANIFEST

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 __author__ = 'Ryan McGrath <ryan@venodesigns.net>'
-__version__ = '1.5.1'
+__version__ = '1.5.2'
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
@@ -20,9 +20,9 @@ setup(
     author='Ryan McGrath',
     author_email='ryan@venodesigns.net',
     license=open('LICENSE').read(),
-    url='https://github.com/ryanmcgrath/twython/tree/master',
+    url='https://github.com/ryanmcgrath/twython-django/tree/master',
     keywords='twitter search api tweet twython stream django integration',
-    description='Actively maintained, pure Python wrapper for the Twitter API. Supports both normal and streaming Twitter APIs',
+    description='Django Integration for Twython -- the actively maintained, pure Python wrapper for the Twitter API. Supports both normal and streaming Twitter APIs',
     long_description=open('README.rst').read(),
     include_package_data=True,
     packages=find_packages(),
